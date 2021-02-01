dc3d29724d6da207cd2d111166bac9d932cfaefa
Gabriel Araujo <gabriiel66@gmail.com>
2021-02-01 20:15:08 -0300

    refact

--- pages/api/shortcuts/[code].js
+++ pages/api/shortcuts/[code].js
@@ -9,48 +9,52 @@ export default async (req, res) => {
 
     // TODO: authorization
 
-    const updated = await firebase
-      .firestore()
-      .collection('shortcuts')
-      .doc(code)
-      .update({
+    try {
+      await firebase.firestore().collection('shortcuts').doc(code).update({
         origin: origin,
       })
 
-    return res.status(200).json({ success: 'Shortcut updated successfully' })
+      return res.status(200).json({ success: 'Shortcut updated successfully' })
+    } catch (err) {
+      return res.status(400).json({ error: err })
+    }
   }
 
   // DELETE
   if (req.method === 'DELETE') {
     // TODO: authorization
 
-    const deleted = await firebase
-      .firestore()
-      .collection('shortcuts')
-      .doc(code)
-      .delete()
+    try {
+      await firebase.firestore().collection('shortcuts').doc(code).delete()
 
-    return res.status(200).json({ success: 'Shortcut deleted successfully' })
+      return res.status(200).json({ success: 'Shortcut deleted successfully' })
+    } catch (err) {
+      return res.status(400).json({ error: err })
+    }
   }
 
   // READ
-  const shortcut = await firebase
-    .firestore()
-    .collection('shortcuts')
-    .doc(code)
-    .get()
+  try {
+    const shortcut = await firebase
+      .firestore()
+      .collection('shortcuts')
+      .doc(code)
+      .get()
 
-  const currentShortcut = shortcut.data()
-  const updatedShortcut = {
-    ...currentShortcut,
-    count: ++currentShortcut.count,
+    const currentShortcut = shortcut.data()
+    const updatedShortcut = {
+      ...currentShortcut,
+      count: ++currentShortcut.count,
+    }
+
+    await firebase
+      .firestore()
+      .collection('shortcuts')
+      .doc(code)
+      .set(updatedShortcut)
+
+    return res.status(200).json(updatedShortcut)
+  } catch (err) {
+    return res.status(400).json({ error: err })
   }
-
-  await firebase
-    .firestore()
-    .collection('shortcuts')
-    .doc(code)
-    .set(updatedShortcut)
-
-  return res.status(200).json(updatedShortcut)
 }
