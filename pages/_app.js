54d9a0f56ce4cda828a89266fe8637ea87d2334f
Gabriel Araujo <gabriiel66@gmail.com>
2021-02-01 22:34:03 -0300

    Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,24 @@
 import { SWRConfig } from 'swr'
+import { ChakraProvider } from '@chakra-ui/react'
+
 import fetcher from 'services/fetcher'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SWRConfig
-      value={{
-        refreshInterval: 0,
-        revalidateOnFocus: false,
-        revalidateOnReconnect: true,
-        refreshWhenHidden: false,
-        refreshWhenOffline: false,
-        fetcher: fetcher,
-      }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
+    <ChakraProvider>
+      <SWRConfig
+        value={{
+          refreshInterval: 0,
+          revalidateOnFocus: false,
+          revalidateOnReconnect: true,
+          refreshWhenHidden: false,
+          refreshWhenOffline: false,
+          fetcher: fetcher,
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </ChakraProvider>
   )
 }
 
