bff3ce57970c9a84c4d63204c5be6955fb483f2f
Gabriel Araujo <gabriiel66@gmail.com>
2021-02-03 20:34:29 -0300

    document meta

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,27 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1.0, initial-scale=1.0, maximum-scale=5.0, width=device-width"
+          />
+          <meta
+            name="application-name"
+            content={process.env.NEXT_PUBLIC_PROJECT_NAME}
+          />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#FFFFFF" />
+          <meta name="browsermode" content="application" />
+          <meta name="full-screen" content="yes" />
+          <meta
+            name="apple-mobile-web-app-title"
+            content={process.env.NEXT_PUBLIC_PROJECT_NAME}
+          />
+          <meta
+            name="description"
+            content={process.env.NEXT_PUBLIC_PROJECT_DESCRIPTION}
+          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
